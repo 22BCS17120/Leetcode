b98cd2416cd14a914e64a37eeba80f2ae05cb06a
22BCS17120 <149155032+22BCS17120@users.noreply.github.com>
2025-02-27 09:52:08 +0530

    Time: 377 ms (25%), Space: 13.5 MB (95.51%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
-        int n = arr.size();
-        // Store array elements in set for O(1) lookup
-        unordered_set<int> numSet(arr.begin(), arr.end());
+        long long int n=arr.size(),ans=0,cnt=0,last=0;
+        for(int i=0;i<n;i++){
+             for(int j=i+1;j<n;j++){
+                 long long int sum=arr[i]+arr[j];
+                 cnt=2;
+                 last=arr[j];
+                 while(binary_search(arr.begin(),arr.end(),sum)){
+                     long long int temp=sum;
+                     sum=last+sum;
+                     last=temp;
+                     cnt++;
+                 }
+                 ans=max(ans,cnt);
 
-        int maxLen = 0;
-        // Try all possible first two numbers of sequence
-        for (int start = 0; start < n; ++start) {
-            for (int next = start + 1; next < n; ++next) {
-                // Start with first two numbers
-                int prev = arr[next];
-                int curr = arr[start] + arr[next];
-                int len = 2;
-
-                // Keep finding next Fibonacci number
-                while (numSet.find(curr) != numSet.end()) {
-                    // Update values for next iteration
-                    int temp = curr;
-                    curr += prev;
-                    prev = temp;
-                    maxLen = max(maxLen, ++len);
-                }
-            }
+             }
         }
-
-        return maxLen;
+        return  ans==2?0:ans;
+        
     }
 };
