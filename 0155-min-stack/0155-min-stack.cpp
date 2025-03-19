4c65ae3817f23083dfdacd4c4e2f6fc629f6f392
22BCS17120 <149155032+22BCS17120@users.noreply.github.com>
2025-03-19 22:00:44 +0530

    Time: 4 ms (45.2%), Space: 23.5 MB (40.89%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,19 +1,20 @@
 class MinStack {
+private:
+    stack<int> s1;
+    stack<int> s2;
 public:
-    vector< pair<int,int> > s;
-	
-    MinStack() { }
-    
-    void push(int val) {
-        if(s.empty())
-            s.push_back({val,val});
-        else
-            s.push_back({val,min(s.back().second,val)});    
+    void push(int x) {
+	    s1.push(x);
+	    if (s2.empty() || x <= getMin())  s2.push(x);	    
     }
-    
-    void pop() { s.pop_back(); }
-    
-    int top() { return s.back().first; }
-    
-    int getMin() { return s.back().second; }
+    void pop() {
+	    if (s1.top() == getMin())  s2.pop();
+	    s1.pop();
+    }
+    int top() {
+	    return s1.top();
+    }
+    int getMin() {
+	    return s2.top();
+    }
 };
